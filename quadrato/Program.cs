d90f3ad94416036469dd57201d893f3edc4da6a3
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2025-11-18 10:35:19 +0100

    Aggiungere i file di progetto.

---
+++ quadrato/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace quadrato
+{
+    internal class Program
+    {
+        static bool quadrato(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) {
+
+               
+                if (array[i]  < array[i - 1] * array[i - 1]) {
+
+                    return true;
+                }
+               if(i == 0)
+                {
+                    return false;
+                }
+
+            }
+          return false;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 5, 9, 10, 77, 6, 34, 8 };
+            Console.WriteLine(quadrato(array));
+        }
+    }
+}
